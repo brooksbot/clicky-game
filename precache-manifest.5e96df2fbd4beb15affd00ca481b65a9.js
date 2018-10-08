ca4a1a37ba9715671af6aa011cfae55dddd5d497
Brooksbot <brooksbailey.bb@gmail.com>
2018-10-08 12:11:23 -0500

    Updates

---
+++ precache-manifest.5e96df2fbd4beb15affd00ca481b65a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "c955ae79c9b7997f440a",
+    "url": "/clicky-game/static/js/main.c955ae79.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clicky-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "c955ae79c9b7997f440a",
+    "url": "/clicky-game/static/css/main.694a0f08.chunk.css"
+  },
+  {
+    "revision": "0d10c5014f758109c5809b3ad9067bb2",
+    "url": "/clicky-game/index.html"
+  }
+];
